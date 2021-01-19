04da7467b71999b16cfc98a65775b482f7754a62
shailendra.baghel <Shailendra.Baghel@IN01N03274>
2021-01-19 20:30:44 +0530

    Add login method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -19,6 +19,23 @@ namespace API.Controllers
 
         }
 
+        [HttpPost("Login")]
+        public async Task<ActionResult<AppUser>> Login([FromBody] LoginDtos loginDtos)
+        {
+            var user = await _dataContext.AppUsers.SingleOrDefaultAsync(x => x.UserName == loginDtos.UserName);
+            if (user is null) return Unauthorized("User is Unauthorized");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var hashPassword = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDtos.Password));
+
+            for (int i = 0; i < hashPassword.Length; i++)
+            {
+                if (hashPassword[i] != user.PasswordHash[i]) return Unauthorized("Password is Invalid");
+            }
+            return user;
+        }
+
         [HttpPost("register")]
         public async Task<ActionResult<AppUser>> Register([FromBody] RegiserDtos regiserDtos)
         {
